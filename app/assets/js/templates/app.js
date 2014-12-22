707e54cfac6bb00a65b20f29640541e304713ffb
Maxim Denisov <denisovmax1988@gmail.com>
2014-12-22 21:15:42 +0300

    Fix Edit template

--- app/assets/js/templates/app.js
+++ app/assets/js/templates/app.js
@@ -196,7 +196,7 @@ angular.module("views/daytheme/edit.tpl.html", []).run(["$templateCache", functi
     "                        <div class=\"form-group\">\n" +
     "                            <label class=\"control-label col-md-2 col-lg-1\">Банковские продукты</label>\n" +
     "                            <div class=\"col-md-5 col-lg-5\">\n" +
-    "                                <input type=\"hidden\" ui-select2=\"select2Options\" ng-model=\"item.products\" />\n" +
+    "                                <input type=\"hidden\" ui-select2=\"select2Options\" ng-model=\"item.products1\" />\n" +
     "                            </div>\n" +
     "                        </div>\n" +
     "                        <div class=\"form-group\">\n" +
